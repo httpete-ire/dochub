5a3df4b96d082b497260da66be9646945a7ee766
Pete Redmond <redmondp@gmail.com>
2015-11-06 15:05:25 +0000

    express middleware
    set up express middleware
    
    * bodyParser to read values from forms etc
    * morgan to log API calls
    * set static directory to ‘public’ directory

--- app/config/initializers/server/index.js
+++ app/config/initializers/server/index.js
@@ -3,14 +3,34 @@
 // public directory
 // express
 // routes
+
+'use strict';
+
 const express = require('express');
 const morgan = require('morgan');
+const chalk = require('chalk');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+let app = null;
+let logger = null;
 
 module.exports =  function(cb) {
-	'use strict';
 
+  app = express();
 
+  app.use(express.static(path.resolve(__dirname,'./../../../../public')));
 
+  if (process.env.NODE_ENV === 'dev') {
+    logger = morgan('dev');
+  } else {
+    logger = morgan('common');
+  }
+
+  app.use(logger);
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json({type: '*/*'}));
 
   console.log(chalk.blue('[SERVER]'));
   cb();
