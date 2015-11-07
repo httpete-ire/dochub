78dfc21bb8d28ff72cfed125da61de7e79f13ef6
Pete Redmond <redmondp@gmail.com>
2015-11-07 20:09:02 +0000

    (feat) generate JWT token

---
+++ app/helpers/auth/auth-token.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var jwt = require('jwt-simple');
+
+module.exports =  function(user, date) {
+  let payload = {
+    user: user,
+    iat: new Date().getTime(),
+    ex: expires(date || 7)
+  };
+
+  return jwt.encode(payload, process.env.SECRET);
+};
+
+function expires(days) {
+
+  days = +days || 0;
+
+  let date = new Date();
+
+  return date.setDate(date.getDate() + days);
+}
