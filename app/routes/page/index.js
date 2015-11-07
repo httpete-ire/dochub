daa4bde0e707226506670b8c0ce60df68376cdbd
Pete Redmond <redmondp@gmail.com>
2015-11-07 19:21:37 +0000

    (chore) add error middleware

--- app/routes/page/index.js
+++ app/routes/page/index.js
@@ -1,7 +1,9 @@
 const routeLoader = require('./../../helpers/route-loader.js');
+const errorMiddleware = require('./../../helpers/middleware/error');
 
 module.exports =  function pageRouteLoader(app) {
   routeLoader(__dirname, app, function(router) {
     app.use(router);
+    app.use(errorMiddleware);
   });
 };
