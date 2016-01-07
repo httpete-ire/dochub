8dc97bb712abcbf107d58e0531b520d523a53b2f
Pete Redmond <redmondp@gmail.com>
2016-01-07 13:53:37 +0000

    (feat) create new document
    Create a new document route complete, the user must be authenticated to
    perform this action.
    
    Also a user document title must be unique, if it is not a 409 error is
    returned to the client

---
+++ app/controllers/docs/index.js
@@ -0,0 +1,3 @@
+module.exports= {
+  post: require('./docs.post.js')
+};
--- app/routes/api/v1/docs.js
+++ app/routes/api/v1/docs.js
@@ -1,7 +1,12 @@
+'use strict';
+
+const docController = require(__base + '/controllers/docs');
+const authMiddleWare = require(__base + '/helpers/middleware/auth');
+
 module.exports =  function(router) {
 
-  router.route('/docs').get(function(req, res) {
-    res.json({ title: 'My first book'});
-  });
+  router
+  .route('/docs')
+  .post(authMiddleWare, docController.post);
 
 };
