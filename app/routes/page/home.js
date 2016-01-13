758425d7e5d5f7bac870e06c9c000806bede1218
Pete Redmond <redmondp@gmail.com>
2016-01-13 17:55:32 +0000

    start of document route and page

--- app/routes/page/home.js
+++ app/routes/page/home.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// '/' render the marketing website
+const Doc = require(__base + 'models/docs');
+const _ = require('lodash');
+
 module.exports =  function(router) {
 
   router.get('/', function(req, res) {
@@ -9,6 +11,44 @@ module.exports =  function(router) {
 
   router.get('/app', function(req, res) {
     res.render('app');
+  });
+
+  router.get('/doc/:id', function(req, res, next) {
+
+    let docId = req.params.id;
+
+    let query = Doc.findOne().where({
+      _id: docId
+    });
+
+    // select certain fields of data
+    if(!_.isEmpty(req.query) && req.query.fields) {
+
+      let fields = req.query.fields.split(',');
+      query.select(fields.join(' '));
+
+      // return all fields of a Doc, populate the owner field
+    }
+
+    query.exec().then(function(doc) {
+      let chapter = (req.query.chapter) ? req.query.chapter : 0;
+      let links = [];
+
+      console.log(req.hostname);
+
+      _.each(doc.chapters, function(docChapter, index) {
+        links.push({
+          title: docChapter.title,
+          href: req.hostname + ':4000/doc/' + doc._id + '/' + index
+        });
+      });
+
+      return res.render('doc', {
+        links: links,
+        chapter: doc.chapters[chapter]
+      });
+    });
+
   });
 
 };
