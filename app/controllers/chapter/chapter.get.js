60886134119c8b2224e74ee0a8b30db0e0664a55
Pete Redmond <redmondp@gmail.com>
2016-01-23 04:44:42 +0000

    (fix) allow user to select fields of chapter resource

--- app/controllers/chapter/chapter.get.js
+++ app/controllers/chapter/chapter.get.js
@@ -1,75 +1,38 @@
 'use strict';
 
 const Doc = require(__base + 'models/docs');
-var jwt = require('jwt-simple');
+const jwt = require('jwt-simple');
 const NotFoundError = require(__base + 'helpers/errors/not-found');
-
-
-const update = {
-  $inc: {
-    'chapters.$.views': 1
-  }
-};
+const _ = require('lodash');
 
 function getChapter(req, res, next) {
 
-  // where statement for query
-  let where = {
+  let query = Doc.findOne({
     _id: req.params.docid,
     'chapters._id': req.params.chapterid
-  };
+  });
 
-  let query;
-
-  // check if a user is logged in to the system
-  // if a user is found and they are the owner of the document/chapter
-  // return the full object including the pullrequest, the view count
-  // is also not incremented
-  if(req.headers.auth) {
-
-    // seperate the token from the bearer
-    let token = req.headers.auth.split(' ')[1];
-
-    try {
-
-      let decodedToken = jwt.decode(token, process.env.SECRET);
-
-      where.owner = decodedToken.user;
-
-      // logged in so dont increment view count
-      query = Doc.findOne(where);
-
-    } catch (err) {
-
-      query = Doc.findOneAndUpdate(where, update, {
-        new: true
-      });
-
-    }
-
-  } else {
-    query = Doc.findOneAndUpdate(where, update, {
-      new: true
-    });
+  if(!_.isEmpty(req.query) && req.query.fields) {
+    let fields = req.query.fields.split(',');
+    console.log(fields);
+    query.select(fields.join(' ') + ' chapters._id');
   }
 
-  if(!where.owner) {
-    query.select('-chapters.pullrequest.content');
-  }
+  query.exec()
+  .then(function(doc) {
 
-  query.exec().then(function(doc) {
-
-    if(doc && doc.chapters) {
-      let chapter = doc.chapters.id(req.params.chapterid);
-      return res.send(chapter);
-    } else {
-      throw new NotFoundError('no chapter resource found with that id');
+    if(!doc) {
+      throw new NotFoundError('no resource found with the id : ' + req.params.docid);
     }
 
+    let chapter = doc.chapters.id(req.params.chapterid);
+
+    return res.send(chapter);
   }).catch(function(err) {
     next(err);
   });
 
 }
+
 
 module.exports = getChapter;
